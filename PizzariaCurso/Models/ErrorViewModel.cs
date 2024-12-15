9314401a0baef9cad4471aa62f4eb6beaa4d0a9c
Tiago Morais <tiagomo1995@gmail.com>
2024-12-15 14:20:50 -0300

    Excluindo arquivos padrao

--- PizzariaCurso/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PizzariaCurso.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
