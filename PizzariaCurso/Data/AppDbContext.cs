dad66cbd6fafd527ada0003e4046321ff48153ee
Tiago Morais <tiagomo1995@gmail.com>
2024-12-17 20:35:40 -0300

    Criando conexao com o banco

---
+++ PizzariaCurso/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzariaCurso.Models;
+
+namespace PizzariaCurso.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<PizzaModel> Pizzas { get; set; }
+    }
+}
